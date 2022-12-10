8c5317e4c9be207040e2339493ae6b97a4cefab9
Francisco Ram Jimenez Velasquez <franciscoramjive@gmail.com>
2022-12-09 21:29:36 -0600

    Chapter 10: ViewSets and Routers

--- BlogAPI/django_project/posts/urls.py
+++ BlogAPI/django_project/posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
 
-from .views import PostList, PostDetail,UserDetail,UserList
 
-urlpatterns = [
-    path("users/", UserList.as_view()),  
-    path("users/pk>/", UserDetail.as_view()),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+urlpatterns = router.urls
--- BlogAPI/django_project/posts/views.py
+++ BlogAPI/django_project/posts/views.py
@@ -1,28 +1,20 @@
 from rest_framework import generics,permissions  
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer,UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):  
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    #permission_classes = (permissions.IsAdminUser,)
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class UserList(generics.ListCreateAPIView):  # new
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):  # new
+class UserViewSet(viewsets.ModelViewSet): 
+    permission_classes = [IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
